9e220c5081c9f11e17d48801814ccb350e185170
Kaj Dreef <kaj.dreef@gmail.com>
2020-10-05 14:57:10 -0700

    Try out for to build the visualization and create a release.

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -5,8 +5,8 @@ class Footer extends React.Component {
     render() {
         return (
             <div class='footer'>
-                <div><a href="http://spideruci.org"><img src="spider_circle_red.svg" /></a></div>
-                <div><a href="https://github.com/spideruci"><img src="./GitHub-Mark-32px.png" /></a></div>
+                <div><a href="http://spideruci.org"><img alt="spideruci.org" src="spider_circle_red.svg" /></a></div>
+                <div><a href="https://github.com/spideruci"><img alt="github.com/spideruci" src="./GitHub-Mark-32px.png" /></a></div>
             </div>    
         )
     }
