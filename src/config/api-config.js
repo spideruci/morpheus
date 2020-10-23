78a806a2a450e2805616a1c6d5833758e8e65dfc
Kaj Dreef <kaj.dreef@gmail.com>
2020-10-22 18:45:51 -0700

    Update api-config.js

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -2,7 +2,7 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === 'spidertest.org') {
+if (hostname === 'http://spidertest.kajdreef.com') {
     backendHost = 'http://api.kajdreef.com';
 } else {
     backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8000';
