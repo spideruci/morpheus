9365da2d2a79bf9caf5b04642d717ad359b5c2ba
Carol Abadeer <cabadeer@uci.edu>
2020-11-18 23:13:09 -0800

    fixed Menu component to reset menus when the reset button is pressed

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Tooltip from '@material-ui/core/Tooltip';
 import HelpIcon from '@material-ui/icons/Help';
 import './Menu.scss'
 
-const Menu = ({ title = "", description=[], onChange=(e) => console.log(e), entries = [] }) => {
+const Menu = ({ title = "", description=[], onChange=(event) => console.log(event.target.value), entries = [], reset, updateReset}) => {
+    const [value, setValue] = useState("default");
+
+    const handleChange = e =>{
+        if(reset === true){
+            updateReset();
+        }
+        setValue(e.target.value) 
+         
+    }
+
     let tooltip = description.length === 0 ? null : (
         <Tooltip
             title={
@@ -13,18 +23,26 @@ const Menu = ({ title = "", description=[], onChange=(e) => console.log(e), entr
             <HelpIcon fontSize="small" />
         </Tooltip>
     )
+    useEffect(() => {
+        if(reset) {
+            setValue("default")
+        }
+      }, [reset]); // Only re-run the effect if reset changes
+
     return (
         <div className="menu-selector">
             <span>{title}: </span>
             {tooltip}
-            <select onChange={onChange}>
-                <option> -- select an option -- </option>
+            <select value={value} onChange={handleChange}>
+                <option value="default"> -- select an option -- </option>
+                <option value="grapefruit">Grapefruit</option>
                 {entries.map((entry) => (
                     <option key={entry.key} value={entry.value}>{entry.value}</option>
+                    
                 ))}
             </select>
         </div>
     )
-}
 
+}
 export default Menu;
