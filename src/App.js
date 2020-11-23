b952c5d09cea411a01f491017598a8f5e0aaf763
Carol Abadeer <cabadeer@uci.edu>
2020-11-23 14:12:43 -0800

    added HistoryMatrixView routes to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import About from './components/routes/About';
 import TestMatrixView from './components/routes/TestMatrixView';
+import HistoryMatrixView from './components/routes/HistoryMatrixView';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import './App.scss';
@@ -19,12 +20,14 @@ function App() {
         <nav>
           <Link to="/about">About</Link>
           <Link to="/visualization">Visualization</Link>
+          <Link to="/history">History</Link>
         </nav>
         <div className="component">
           <Switch>
             <Route exact path="/" component={About} />
             <Route exact path="/about" component={About} />
             <Route exact path="/visualization" component={TestMatrixView} />
+            <Route exact path="/history" component={HistoryMatrixView} />
           </Switch>
         </div>
       </Router>
